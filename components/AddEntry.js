3e374eb4ffa8d165fa4d70d6de479966da43ccdb
andrewShillito <34523587+A-S-8@users.noreply.github.com>
2019-01-07 14:17:24 -0500

    feat: finish AddEntry slider, increment/decrement funcs

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -30,6 +30,11 @@ export default class AddEntry extends Component {
       }
     });
   }
+  slide = (metric, value) => {
+    this.setState(() => ({
+      [metric]: value,
+    }))
+  }
   render() {
     return (
       <View>
