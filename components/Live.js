f71a91dada1ade4ef9f68ece5477dc200e7b03c8
andrewShillito <34523587+A-S-8@users.noreply.github.com>
2019-01-14 11:08:27 -0500

    feat: continue building Live tab

--- components/Live.js
+++ components/Live.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
-import { View, Text, ActivityIndicator } from "react-native";
+import { View, Text, ActivityIndicator, TouchableOpacity, StyleSheet } from "react-native";
+import { Foundation } from "@expo/vector-icons";
+import { purple, white } from "../utils/colors";
 
 export default class Live extends Component {
   state = {
     coords: null,
-    status: null,
+    status: "undetermined",
     direction: '',
+  }
+  askPermission = () => {
+
   }
   render() {
     const { status, coords, direction } = this.state;
@@ -16,16 +21,28 @@ export default class Live extends Component {
 
     if (status === "denied") {
       return (
-        <View>
-          <Text>Denied</Text>
+        <View style={styles.center}>
+          <Foundation name="alert" size={50}/>
+          <Text>Location permissions are required for this feature</Text>
+          <TouchableOpacity onPress={this.askPermission} style={styles.button}>
+            <Text style={styles.buttonText}>
+              Enable
+            </Text>
+          </TouchableOpacity>
         </View>
       );
     }
 
     if (status === "undetermined") {
       return (
-        <View>
-          <Text>undetermined</Text>
+        <View style={styles.center}>
+          <Foundation name="alert" size={50}/>
+          <Text>Location services are required for this feature</Text>
+          <TouchableOpacity onPress={this.askPermission} style={styles.button}>
+            <Text style={styles.buttonText}>
+              Enable
+            </Text>
+          </TouchableOpacity>
         </View>
       );
     }
@@ -38,3 +55,28 @@ export default class Live extends Component {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "space-between",
+  },
+  center: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginLeft: 30,
+    marginRight: 30,
+  },
+  button: {
+    padding: 10,
+    backgroundColor: purple,
+    alignSelf: "center",
+    borderRadius: 5,
+    margin: 20,
+  },
+  buttonText: {
+    color: white,
+    fontSize: 20,
+  },
+});
